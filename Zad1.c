f4af193de4e0545747c28252335def80f6790df0
lorosn <lorenorosnjak1@gmail.com>
2024-11-12 16:37:00 +0100

    Update Zad1.c

--- Zad1.c
+++ Zad1.c
@@ -1,59 +1,134 @@
-#define MAX_BOD 100.0
 #define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 
-typedef struct {
-    char ime[50];
-    char prez[50];
-    float bod;
-} student;
+typedef struct Node {
+	double data;
+	struct Node* next;
+} Node;
 
-int main()
-{
-    student* stud, tren;
-    int n = 0;
+int initializeStack(Node** stack);
+int push(Node** stack, double value);
+int pop(Node** stack, double* result);
+double evaluatePostfix(FILE* filePointer);
 
-    stud = malloc(sizeof(student));
-    if (stud == NULL) {
-        printf("Greska pri alokaciji memorije!\n");
-        return 1;
-    }
-    printf("Uspesno alocirana pocetna memorija.\n");
+int main() {
+	FILE* file = fopen("postfix.txt", "r");
+	if (file == NULL) {
+		printf("File opening error!\n");
+		return EXIT_FAILURE;
+	}
+	double result = evaluatePostfix(file);
 
-    FILE* fp = fopen("Studenti.txt", "r");
-    if (fp == NULL) {
-        printf("Greska pri otvaranju datoteke!\n");
-        free(stud);
-        return 1;
-    }
+	printf("Rezultat of postfix term: %.2lf\n", result);
 
-    while (fgetc(fp) != '\n');
+	fclose(file);
 
-    while (fscanf(fp, "%s %s %f", tren.ime, tren.prez, &tren.bod) == 3)
-    {
-        student* temp = realloc(stud, (n + 1) * sizeof(student));
-        if (temp == NULL) {
-            printf("Greska pri realokaciji memorije!\n");
-            free(stud);
-            fclose(fp);
-            return 1;
-        }
-        stud = temp;
-        stud[n] = tren;
-        n++;
-    }
+	return EXIT_SUCCESS;
+}
 
-    fclose(fp);
+int initializeStack(Node** stack) {
+	*stack = NULL;
+	return 1;
+}
 
-    printf("Ime\t\tPrezime\t\tBodovi\t\tRelativni Bodovi\n");
-    for (int i = 0; i < n; i++) {
-        float relBod = (stud[i].bod / MAX_BOD) * 100;
-        printf("%s %s %.2f\t\t%.2f%%\n", stud[i].ime, stud[i].prez, stud[i].bod, relBod);
-    }
+int push(Node** stack, double value) {
+	Node* newNode = (Node*)malloc(sizeof(Node));
+	if (!newNode) {
+		printf("Error allocating memory!\n");
+		return 0;
+	}
 
-    free(stud);
-    return 0;
+	newNode->data = value;
+	newNode->next = *stack;
+	*stack = newNode;
+
+	return 1;
+}
+
+int pop(Node** stack, double* result) {
+	if (!*stack) {
+		printf("Stack is empty!\n");
+		return 0;
+	}
+
+	Node* top = *stack;
+	*result = top->data;
+	*stack = top->next;
+	free(top);
+
+	return 1;
+}
+
+double evaluatePostfix(FILE* filePointer) {
+	Node* stack;
+	if (!initializeStack(&stack)) {
+		printf("Stack initialization error!\n");
+		return EXIT_FAILURE;
+	}
+
+	char token[20];
+	while (fscanf(filePointer, "%s", token) != EOF) {
+		if (token[0] >= '0' && token[0] <= '9') {
+			double value = atof(token);
+			if (!push(&stack, value)) {
+				printf("Adding stack element error.\n");
+				return EXIT_FAILURE;
+			}
+		}
+		else {
+			double operand2, operand1;
+			if (!pop(&stack, &operand2) || !pop(&stack, &operand1)) {
+				printf("Removing stack element error.\n");
+				return EXIT_FAILURE;
+			}
+
+			switch (token[0]) {
+			case '+':
+				if (!push(&stack, operand1 + operand2)) {
+					printf("Error adding result back on stack.\n");
+					return EXIT_FAILURE;
+				}
+				break;
+			case '-':
+				if (!push(&stack, operand1 - operand2)) {
+					printf("Error adding result back on stack.\n");
+					return EXIT_FAILURE;
+				}
+				break;
+			case '*':
+				if (!push(&stack, operand1 * operand2)) {
+					printf("Error adding result back on stack.\n");
+					return EXIT_FAILURE;
+				}
+				break;
+			case '/':
+				if (operand2 == 0) {
+					printf("Division by zero isn't allowed!\n");
+					return EXIT_FAILURE;
+				}
+				if (!push(&stack, operand1 / operand2)) {
+					printf("Error adding result back on stack.\n");
+					return EXIT_FAILURE;
+				}
+				break;
+			default:
+				printf("Unknown operator: %s\n", token);
+				return EXIT_FAILURE;
+			}
+		}
+	}
+
+	double result;
+	if (!pop(&stack, &result)) {
+		printf("Invalid postfix term.\n");
+		return EXIT_FAILURE;
+	}
+
+	if (stack != NULL) {
+		printf("Invalid postfix term.\n");
+		return EXIT_FAILURE;
+	}
+
+	return result;
 }
